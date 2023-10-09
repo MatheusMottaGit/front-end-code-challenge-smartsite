4809fee1a896f465dd7689b0336f544315a588a7
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-09 08:09:05 -0300

    feat: tailwind init

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ export default {
         'gotham-bold': "url('assets/fonts/gotham-bold.woff2')",
         'gotham-book': "url('assets/fonts/gotham-book.woff2')",
         'gotham-light': "url('assets/fonts/gotham-light.woff2')"
+      },
+
+      colors: {
+       'dark-grey': '#333333',
+       'light-grey': '#808080',
+       'yellow': '#FFB612',
+       'red': '#dc0a17',
+       'green': '#2FC022'
       }
     },
   },
