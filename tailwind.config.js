971c0296590134481169e552c77f56c562b93bfd
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-08 22:05:51 -0300

    feat: setting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gotham: {
+          black: "url('assets/fonts/gotham-black.woff2')",
+          bold: "url('assets/fonts/gotham-bold.woff2')",
+          book: "url('assets/fonts/gotham-book.woff2')",
+          light: "url('assets/fonts/gotham-light.woff2')"
+        }
+      }
+    },
   },
   plugins: [],
 }
