ac02a875cd4756ce8ff833b1db42d3df6776e900
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-08 22:13:15 -0300

    feat: tailwind init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.tsx"
+  ],
   theme: {
     extend: {
       fontFamily: {
-        gotham: {
-          black: "url('assets/fonts/gotham-black.woff2')",
-          bold: "url('assets/fonts/gotham-bold.woff2')",
-          book: "url('assets/fonts/gotham-book.woff2')",
-          light: "url('assets/fonts/gotham-light.woff2')"
-        }
+        'gotham-black': "url('assets/fonts/gotham-black.woff2')",
+        'gotham-bold': "url('assets/fonts/gotham-bold.woff2')",
+        'gotham-book': "url('assets/fonts/gotham-book.woff2')",
+        'gotham-light': "url('assets/fonts/gotham-light.woff2')"
       }
     },
   },
