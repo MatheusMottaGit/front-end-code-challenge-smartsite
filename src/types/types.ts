373ad4cce44b6327f4752d8240de5fdfde5ed7fe
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-10 19:17:55 -0300

    feat: setting global types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,19 +1,21 @@
 import { PeriodsValues } from "@/App"
 
-export type Period = {
-  id: string
-  period: string
-  hours: string
-  value: PeriodsValues
+export interface Period {
+  id: string,
+  period: string,
+  hours: string,
+  value: PeriodsValues,
 }
 
-export type Unit = {
-  id: number
-  title: string
-  content: string
-  opened: boolean
-  schedules: [
-    weekdays: string,
-    hour: string
-  ]
+interface Schedule {
+  weekdays: string,
+  hour: string,
+}
+
+export interface Location {
+  id: number,
+  title: string,
+  content: string,
+  opened: boolean,
+  schedules: Schedule[]
 }
